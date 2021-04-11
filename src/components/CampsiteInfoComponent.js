f09fc452fcaab79ab9d88ff2019b450924a8ce81
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-04-11 22:16:44 +0100

    comment rendering

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class CampsiteInfo extends Component {
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 col-m1 ">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+     
+  renderComments(comments){
+      if(comments){
+          return <div class = 'col-md-5 col-m1'>
+             <h4>Comments</h4>
+              {comments.map(comment => {
+                  return(<div key = {comment.id}>
+                      <p>{comment.text}
+                      <br/> 
+                      {comment.author}
+                      {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                       </p>
+                  </div>)    
+               })}
+          </div>
+        }
+    }
+
+  render() {
+    if (this.props.campsiteinfo) {
+      return (
+         <div className="row">
+             {this.renderCampsite(this.props.campsiteinfo)}
+             {this.renderComments(this.props.campsitecomments)}
+         </div>
+       )
+    }
+    return <div></div>;
+  }
+}
+
+export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
+
 
 class Directory extends Component {
     constructor(props) {
@@ -11,21 +13,6 @@ class Directory extends Component {
 
     onCampsiteSelect(campsite) {
         this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
     }
 
     render() {
@@ -47,11 +34,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsiteinfo = {this.state.selectedCampsite}/>
             </div>
         );
     }
