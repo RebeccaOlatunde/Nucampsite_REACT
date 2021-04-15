54d8d270eb048b66f967e0c02069123de41b9861
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-04-15 22:18:49 +0100

    Adding to the single page app

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+const HomePage = () => {
+    return (
+        <Home
+            campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+            promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+            partner={this.state.partners.filter(partner => partner.featured)[0]}
+        />
+    );
+};
 
 function Home(props) {
     return (
         <div className="container">
-            <h4>Home</h4>
+            <div className="row">
+                <div className="col-md m-1">
+                    <RenderCard item={props.campsite} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
+                </div>
+            </div>
         </div>
     );
 }
-
 export default Home;   
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,11 @@ import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 
 
@@ -14,15 +18,22 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render(){
         const HomePage = () => {
             return (
-                <Home/>
-            )
-        }
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        };
    
         return (
             <div>
@@ -30,6 +41,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
