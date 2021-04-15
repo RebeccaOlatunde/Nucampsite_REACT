57e267c3cdc5d18ffcbb4325ce23475d9be72819
rebeccaolatunde <olatunde.rebecca@gmail.com>
2021-04-15 16:50:07 +0100

    React Router

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,12 +1,13 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}){
     return (
-        <Card onClick={() => onClick(campsite.id)}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <Card>
+           <CardImg width = "100%" src={campsite.image} alt={campsite.name}/>
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
             </CardImgOverlay>
+                 
         </Card>
     );
 }
@@ -16,7 +17,7 @@ function Directory(props) {
     const directory = props.campsites.map(campsite => {
         return (
             <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite={campsite}/>
             </div>
         );
     });
